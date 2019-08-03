cb2d5e853b3ac5712bfdfc23e4e8fccf2f30962e
zhangyx <z13520546697@163.com>
2019-08-03 10:59:24 +0800

    123

---
+++ src/main/java/com/fh/shop/vo/qee.java
@@ -0,0 +1,18 @@
+package com.fh.shop.vo;
+
+/**
+ * <pre>包名称：com.fh.shop.vo
+ * 类名称：qee
+ * 类描述：TODO
+ * 创建人：Mr.zhang   2424968072@qq.com
+ * 创建时间：2019/8/310:53
+ * 修改人：Mr.zhang     2424968072@qq.com
+ * 修改备注：
+ * @version </pre>
+ */
+public class qee {
+
+
+     String a = "123";
+
+}
